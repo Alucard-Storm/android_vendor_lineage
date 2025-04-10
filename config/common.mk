dab461fc3990648994ad2a7b5478fa0217d35555
ryzenforce990 <ryzenforce990@users.noreply.github.com>
2025-04-10 11:07:41 +0000

    config: common: Tune dexopt
    Signed-off-by: ryzenforce990 <ryzenforce990@users.noreply.github.com>
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -277,6 +277,40 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/lineage/build/target/product/security/lineage
 
+# Art
+PRODUCT_PRODUCT_PROPERTIES += \
+    pm.dexopt.post-boot=extract \
+    pm.dexopt.boot-after-mainline-update=verify \
+    pm.dexopt.install=speed-profile \
+    pm.dexopt.install-fast=skip \
+    pm.dexopt.install-bulk=speed-profile \
+    pm.dexopt.install-bulk-secondary=verify \
+    pm.dexopt.install-bulk-downgraded=verify \
+    pm.dexopt.install-bulk-secondary-downgraded=extract \
+    pm.dexopt.bg-dexopt=speed-profile \
+    pm.dexopt.ab-ota=speed-profile \
+    pm.dexopt.inactive=verify \
+    pm.dexopt.cmdline=verify \
+    pm.dexopt.shared=quicken \
+    pm.dexopt.first-boot=verify \
+    pm.dexopt.boot-after-ota=verify \
+    dalvik.vm.minidebuginfo=false \
+    dalvik.vm.dex2oat-minidebuginfo=false
+    dalvik.vm.dex2oat-minidebuginfo=false \
+    pm.dexopt.downgrade_after_inactive_days=10 \
+    dalvik.vm.madvise-random=true
+
+# Always preopt extracted APKs to prevent extracting out of the APK for gms
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
+# Use a profile based boot image for this device. Note that this is currently a
+# generic profile and not Android Go optimized.
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
+
 # Paranoid Sense
 ifneq ($(TARGET_FACE_UNLOCK_SUPPORTED),false)
 PRODUCT_PACKAGES += \
